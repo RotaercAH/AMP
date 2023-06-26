9cba2052742cb9212d329ed92a553bb6bfad11c7
RotaercAH <gaoyuanpeng@126.com>
2023-06-26 18:28:06 +0800

    添加数据库

--- backend/src/test/java/com/amp/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/amp/backend/BackendApplicationTests.java
@@ -2,12 +2,19 @@ package com.amp.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.thymeleaf.util.ObjectUtils;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("pw"));
+        System.out.println(passwordEncoder.encode("pb"));
+        System.out.println(passwordEncoder.encode("pc"));
     }
 
 }
