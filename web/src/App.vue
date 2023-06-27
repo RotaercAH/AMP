c2d9c5982f060a19052c619a67d885d6f5f8acfa
RotaercAH <gaoyuanpeng@126.com>
2023-06-27 21:02:00 +0800

    实现用户注册与登录

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,9 +8,28 @@ import NavBar from './components/NavBar.vue';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
 
+
 export default {
   components: {
     NavBar
+  },
+  setup() {
+    // $.ajax({
+    //   url: "http://127.0.0.1:8080/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: 'syr1',
+    //     password: '123',
+    //     confirmedPassword: '123',
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
   }
 }
 </script>
