4acb1b86d735e0d19ab82dc3b0e3b71add06538a
RotaercAH <gaoyuanpeng@126.com>
2023-07-07 10:44:41 +0800

    完成匹配对战系统

---
+++ backendcould/matchingsystem/src/main/java/com/amp/matchingsystem/service/impl/utils/MatchingPool.java
@@ -0,0 +1,120 @@
+package com.amp.matchingsystem.service.impl.utils;
+
+import com.amp.matchingsystem.service.MatchingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Component
+public class MatchingPool extends Thread{
+    private static List<Player> players = new ArrayList<>();
+    private ReentrantLock lock = new ReentrantLock();
+    private static RestTemplate restTemplate;
+    private static final String startGameUrl = "http://127.0.0.1:8080/pk/start/game/";
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        MatchingPool.restTemplate = restTemplate;
+    }
+
+    public void addPlayer(Integer userId, Integer rating){
+        lock.lock();
+        try {
+            players.add(new Player(userId, rating, 0));
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void removePlayer(Integer userId){
+        lock.lock();
+        try {
+           List<Player> newPlayers = new ArrayList<>();
+           for (Player player : players) {
+               if(!player.getUserId().equals(userId)){
+                   newPlayers.add(player);
+               }
+           }
+           players = newPlayers;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void increaseWaitingTime() {
+        for (Player player: players) {
+            player.setWaitingTime(player.getWaitingTime() + 1);
+        }
+    }
+
+    private boolean checkMatched(Player a, Player b) { // 判断两名玩家是否匹配
+        int ratingDelta = Math.abs(a.getRating() - b.getRating());
+        int waitingTime = Math.min(a.getWaitingTime(), b.getWaitingTime());
+        if(a.getUserId().equals(b.getUserId())){
+            if(a.getWaitingTime() > b.getWaitingTime())
+                removePlayer(a.getUserId());
+            else removePlayer(b.getUserId());
+            System.out.println(a.getUserId() + " " + b.getUserId());
+            return false;
+        }
+        return ratingDelta <= waitingTime * 10;
+    }
+
+    private void sendResult(Player a, Player b){ // 返回匹配结果
+        System.out.println("send result: " + a + " " + b);
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("a_id", a.getUserId().toString());
+        data.add("b_id", b.getUserId().toString());
+        restTemplate.postForObject(startGameUrl, data, String.class);
+    }
+
+    private void matchPlayer() { //尝试匹配所有玩家
+        System.out.println("match players: " + players.toString());
+        boolean[] used = new boolean[players.size()];
+        for (int i = 0; i < players.size(); i++) {
+            if (used[i]) continue;
+            for(int j = i + 1; j < players.size(); j++) {
+                if (used[j]) continue;
+                Player a = players.get(i), b = players.get(j);
+                if(checkMatched(a, b)) {
+                    used[i] = used[j] = true;
+                    sendResult(a, b);
+                    break;
+                }
+            }
+        }
+
+        List<Player> newPlayers = new ArrayList<>();
+        for(int i = 0; i < players.size(); i ++){
+            if(!used[i]) {
+                newPlayers.add(players.get(i));
+            }
+        }
+        players = newPlayers;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                lock.lock();
+                try {
+                    increaseWaitingTime();
+                    matchPlayer();
+                } finally {
+                    lock.unlock();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
